ec83a861dfd3fe5fe091e928b427c50258b16f1a
noblebright <noblebright@users.noreply.github.com>
2022-01-10 16:36:19 -0800

    Added special case for inventory naming

--- src/hooks/useLocale.js
+++ src/hooks/useLocale.js
@@ -3,6 +3,10 @@ import { useCallback } from "react";
 export default function useLocale(lang, ruleset) {
     return useCallback((str) => {
         const strings = ruleset.languages[lang];
+        const invKey = ruleset.entries[str]?.items?.name; //special name key for inventory items
+        if(invKey && strings[invKey]) {
+            return strings[invKey];
+        } 
         if(!strings || !strings[str]) {
             return str;
         }
