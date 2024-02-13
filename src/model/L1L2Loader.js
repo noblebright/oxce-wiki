24bfed7569588a0c29ea25c85e1a850442377d8a
noblebright <noblebright@users.noreply.github.com>
2024-02-12 21:49:02 -0800

    Better YAML error handling

--- src/model/L1L2Loader.js
+++ src/model/L1L2Loader.js
@@ -4,163 +4,172 @@ import GithubLoader from "./GithubLoader.js";
 import { getModuleSupportedLanguages, loadText } from "./utils.js";
 
 function rewriteFilePaths(ruleset, loader, sha, foldCase) {
-    const rewriter = entry => {
-        if(entry.fileSingle) {
-            const path = entry.fileSingle;
-            entry.fileSingle = loader.getUrl(sha, path, foldCase); //rewrite to github raw content url
-        } else {
-            Object.keys(entry.files ?? []).forEach(key => {
-                const path = entry.files[key];
-                entry.files[key] = loader.getUrl(sha, path, foldCase); //rewrite to github raw content url
-            });
-        }
+  const rewriter = (entry) => {
+    if (entry.fileSingle) {
+      const path = entry.fileSingle;
+      entry.fileSingle = loader.getUrl(sha, path, foldCase); //rewrite to github raw content url
+    } else {
+      Object.keys(entry.files ?? []).forEach((key) => {
+        const path = entry.files[key];
+        entry.files[key] = loader.getUrl(sha, path, foldCase); //rewrite to github raw content url
+      });
     }
-    ruleset.extraSprites && ruleset.extraSprites.forEach(rewriter);
-    ruleset.extraSounds && ruleset.extraSounds.forEach(rewriter);
+  };
+  ruleset.extraSprites && ruleset.extraSprites.forEach(rewriter);
+  ruleset.extraSounds && ruleset.extraSounds.forEach(rewriter);
 }
 
 function rewriteLocalization(ruleset) {
-    if(!ruleset.extraStrings) return;
-    ruleset.extraStrings.forEach(({type, strings}) => {
-        if(!ruleset[type]) {
-            ruleset[type] = {};
-        }
-        Object.assign(ruleset[type], strings);
-    });
+  if (!ruleset.extraStrings) return;
+  ruleset.extraStrings.forEach(({ type, strings }) => {
+    if (!ruleset[type]) {
+      ruleset[type] = {};
+    }
+    Object.assign(ruleset[type], strings);
+  });
 }
 
 async function loadRuleset({ repo, branch, path }, db, callback) {
-    callback && callback(["LOADING_VERSIONS", repo]);
-    const versions = await db.getVersions(repo, branch);
-    if(!versions[branch]?.sha) {
-        throw new Error(`Unknown module ref: ${repo}@${branch}`);
-    }
+  callback && callback(["LOADING_VERSIONS", repo]);
+  const versions = await db.getVersions(repo, branch);
+  if (!versions[branch]?.sha) {
+    throw new Error(`Unknown module ref: ${repo}@${branch}`);
+  }
 
-    // get commit sha
-    const sha = versions[branch].sha;
+  // get commit sha
+  const sha = versions[branch].sha;
 
-    // check L1
-    callback && callback(["LOADING_FILELIST", repo, sha, 1]);
-    const ruleset = await db.getRuleset(sha);
-    if(ruleset) {
-        console.log(`Found pre-computed ruleset for ${repo}@${branch}`);
-        callback && callback(["LOADING_FILELIST_COMPLETE", repo, sha, 1]);
-        return YamlService.hydrate(ruleset);
-    }
-
-    console.log(`L1 cache miss for ${repo}@${branch}`);
-    const [languageFiles, ruleFiles, foldCase] = await db.getFileList(repo, sha, path);
-    const files = [...Object.entries(languageFiles), ...Object.entries(ruleFiles)];
-    
-    //fetch all available files from L2
-    const cacheFetch = await db.getL2(files.map(([fileSha, url]) => fileSha));
-    const cacheMisses = [];
-    
-    const loader = new GithubLoader(repo);
-
-    callback && callback(["LOADING_FILELIST", repo, sha, cacheFetch.length]);
-    const promises = cacheFetch.map(async (record, i) => {
-        const [fileSha, url] = files[i];
-
-        // found in L2, so use that.
-        if(record)  {
-            callback && callback(["LOADING_FILE", url, 1]);
-            return YamlService.hydrate(record.data);
-        }
-
-        //Fill in cache misses with network calls
-        console.log(`cache miss on ${url}`);
-        const text = await loadText(url);
-        const result = YamlService.parse(text);
-        if(!result) {
-            console.warn(`Skipping empty file: ${url}`);
-            callback && callback(["LOADING_FILE", url, 1]);
-            return {};
-        } else {
-            const [rules] = result; // result is dehydrated, pull out the rules part for rewriting
-
-            // mutate ruleset as necessary
-            rewriteLocalization(rules);
-            rewriteFilePaths(rules, loader, sha, foldCase);
-
-            // only cache successfully parsed files
-            cacheMisses.push({ fileSha, data: result, lastUsed: Date.now()});
-        }
-        callback && callback(["LOADING_FILE", url, 1]);
-        return YamlService.hydrate(result);
-    });
-
-    const parsedFiles = await Promise.all(promises);
-    
-    // write new entries into L2, but don't wait.
-    db.putL2(cacheMisses);
-
-    console.log(`generating ruleset for ${repo}@${sha}`);
-    const rawRuleset = YamlService.mergeList(parsedFiles);
-    
-    // write new entry into L1, but don't wait
+  // check L1
+  callback && callback(["LOADING_FILELIST", repo, sha, 1]);
+  const ruleset = await db.getRuleset(sha);
+  if (ruleset) {
+    console.log(`Found pre-computed ruleset for ${repo}@${branch}`);
     callback && callback(["LOADING_FILELIST_COMPLETE", repo, sha, 1]);
-    db.putL1(sha, YamlService.dehydrate(rawRuleset));
-    return rawRuleset;
+    return YamlService.hydrate(ruleset);
+  }
+
+  console.log(`L1 cache miss for ${repo}@${branch}`);
+  const [languageFiles, ruleFiles, foldCase] = await db.getFileList(
+    repo,
+    sha,
+    path
+  );
+  const files = [
+    ...Object.entries(languageFiles),
+    ...Object.entries(ruleFiles),
+  ];
+
+  //fetch all available files from L2
+  const cacheFetch = await db.getL2(files.map(([fileSha, url]) => fileSha));
+  const cacheMisses = [];
+
+  const loader = new GithubLoader(repo);
+
+  callback && callback(["LOADING_FILELIST", repo, sha, cacheFetch.length]);
+  const promises = cacheFetch.map(async (record, i) => {
+    const [fileSha, url] = files[i];
+
+    // found in L2, so use that.
+    if (record) {
+      callback && callback(["LOADING_FILE", url, 1]);
+      return YamlService.hydrate(record.data);
+    }
+
+    //Fill in cache misses with network calls
+    console.log(`cache miss on ${url}`);
+    const text = await loadText(url);
+    const result = YamlService.parse(text, url);
+    if (!result?.[0]) {
+      console.warn(`Skipping empty file: ${url}`);
+      callback && callback(["LOADING_FILE", url, 1]);
+      return {};
+    } else {
+      const [rules] = result; // result is dehydrated, pull out the rules part for rewriting
+
+      // mutate ruleset as necessary
+      rewriteLocalization(rules);
+      rewriteFilePaths(rules, loader, sha, foldCase);
+
+      // only cache successfully parsed files
+      cacheMisses.push({ fileSha, data: result, lastUsed: Date.now() });
+    }
+    callback && callback(["LOADING_FILE", url, 1]);
+    return YamlService.hydrate(result);
+  });
+
+  const parsedFiles = await Promise.all(promises);
+
+  // write new entries into L2, but don't wait.
+  db.putL2(cacheMisses);
+
+  console.log(`generating ruleset for ${repo}@${sha}`);
+  const rawRuleset = YamlService.mergeList(parsedFiles);
+
+  // write new entry into L1, but don't wait
+  callback && callback(["LOADING_FILELIST_COMPLETE", repo, sha, 1]);
+  db.putL1(sha, YamlService.dehydrate(rawRuleset));
+  return rawRuleset;
 }
 
 export async function load(version, compiler, callback) {
-    console.time("fullLoad");
-    const db = new L1L2DB();
-    await db.connect();
+  console.time("fullLoad");
+  const db = new L1L2DB();
+  await db.connect();
 
-    callback && callback(["LOADING_CONFIG"]);
-    const config = await db.getConfig();
-    const modules = config.modules;
-    modules.at(-1).branch = version; // patch in the version we want for the last module.
+  callback && callback(["LOADING_CONFIG"]);
+  const config = await db.getConfig();
+  const modules = config.modules;
+  modules.at(-1).branch = version; // patch in the version we want for the last module.
 
-    let rulesList;
-    try {
-        rulesList = await Promise.all(modules.map(m => loadRuleset(m, db, callback)));
-    } catch (e) {
-        // If parsing goes south, delete the db.
-        console.error(e);
-        await db.delete();
-        callback && callback(["ERROR", e]);
-        console.timeEnd("fullLoad");
-        throw e;
-    }
-
-    const supportedLanguages = getModuleSupportedLanguages(rulesList);
-    console.log(`supported languages:`, supportedLanguages);
-    callback && callback(["COMPILING_RULESET"]);
-    await new Promise(r => setTimeout(r, 16)); //yield so react can update the progress bar
-    const ruleset = compiler(rulesList, supportedLanguages);
+  let rulesList;
+  try {
+    rulesList = await Promise.all(
+      modules.map((m) => loadRuleset(m, db, callback))
+    );
+  } catch (e) {
+    // If parsing goes south, delete the db.
+    console.error(e);
+    await db.delete();
+    callback && callback(["ERROR", e]);
     console.timeEnd("fullLoad");
-    return { config, supportedLanguages, ruleset };
+    throw e;
+  }
+
+  const supportedLanguages = getModuleSupportedLanguages(rulesList);
+  console.log(`supported languages:`, supportedLanguages);
+  callback && callback(["COMPILING_RULESET"]);
+  await new Promise((r) => setTimeout(r, 16)); //yield so react can update the progress bar
+  const ruleset = compiler(rulesList, supportedLanguages);
+  console.timeEnd("fullLoad");
+  return { config, supportedLanguages, ruleset };
 }
 
 export async function updateLanguage(value) {
-    const db = new L1L2DB();
-    await db.connect();
-    return db.config.put({ key: "currentLanguage", value });
+  const db = new L1L2DB();
+  await db.connect();
+  return db.config.put({ key: "currentLanguage", value });
 }
-  
-export async function clearDB() {
-    const db = new L1L2DB();
-    await db.connect();
-    await db.delete();
-    window.location.reload();
-}
-  
-export async function getMetadata(callback) {
-    const db = new L1L2DB(callback);
-    await db.connect();
-    let result;
-    try {
-        const config = await db.getConfig();
-        const currentLanguage = await db.getCurrentLanguage();
-        const { repo, branch } = config.modules[config.modules.length - 1];
-        const modVersions = await db.getVersions(repo, branch);
-        result = { modVersions, config, currentLanguage, defaultVersion: branch };
-    } catch (e) {
-        await db.delete();
-    }
 
-    return result;
+export async function clearDB() {
+  const db = new L1L2DB();
+  await db.connect();
+  await db.delete();
+  window.location.reload();
+}
+
+export async function getMetadata(callback) {
+  const db = new L1L2DB(callback);
+  await db.connect();
+  let result;
+  try {
+    const config = await db.getConfig();
+    const currentLanguage = await db.getCurrentLanguage();
+    const { repo, branch } = config.modules[config.modules.length - 1];
+    const modVersions = await db.getVersions(repo, branch);
+    result = { modVersions, config, currentLanguage, defaultVersion: branch };
+  } catch (e) {
+    await db.delete();
+  }
+
+  return result;
 }
--- src/model/YamlService.js
+++ src/model/YamlService.js
@@ -4,55 +4,61 @@ import deepmerge from "deepmerge";
 const tags = ["!add", "!remove"];
 
 const symbolToTag = {};
-tags.forEach(tag => symbolToTag[Symbol.for(tag)] = tag);
+tags.forEach((tag) => (symbolToTag[Symbol.for(tag)] = tag));
 
 const tagDef = {
   resolve: () => true,
   instanceOf: Object,
-  represent: (data) => data
+  represent: (data) => data,
 };
 
-const types = tags.map(tag => {
-  const construct = data => { 
-    data[Symbol.for(tag)] = true; 
-    return data; 
-  };
+const types = tags
+  .map((tag) => {
+    const construct = (data) => {
+      data[Symbol.for(tag)] = true;
+      return data;
+    };
 
-  return [
-    new yaml.Type(tag, { ...tagDef, construct, kind: "mapping" }),
-    new yaml.Type(tag, { ...tagDef, construct, kind: "sequence" })
-  ];
-}).flat();
+    return [
+      new yaml.Type(tag, { ...tagDef, construct, kind: "mapping" }),
+      new yaml.Type(tag, { ...tagDef, construct, kind: "sequence" }),
+    ];
+  })
+  .flat();
 
 export const schema = yaml.JSON_SCHEMA.extend(types);
 
 export const customMerge = () => (a, b) => {
-  if(Array.isArray(a) && Array.isArray(b)) {
-    if(b[Symbol.for("!add")]) {
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if (b[Symbol.for("!add")]) {
       return a.concat(b);
-    } else if(b[Symbol.for("!remove")]) {
-      return a.filter(x => !b.includes(x));
+    } else if (b[Symbol.for("!remove")]) {
+      return a.filter((x) => !b.includes(x));
     } else {
       return b;
     }
   } else if (typeof a === "object" && typeof b === "object") {
-    if(b[Symbol.for("!add")]) {
+    if (b[Symbol.for("!add")]) {
       return deepmerge(a, b, { customMerge });
-    } else if(b[Symbol.for("!remove")]) {
-      Object.keys(b).forEach(k => {
+    } else if (b[Symbol.for("!remove")]) {
+      Object.keys(b).forEach((k) => {
         delete a[k];
-      })
+      });
       return a;
     } else {
       return b;
     }
   }
   return b;
-}
+};
 
-export const mergeSection = (ruleset, name, sectionName, entry) => Object.assign({}, deepmerge(ruleset[name][sectionName], entry, { clone: false, customMerge }));
-export const mergeList = list => list.reduce((acc, obj) => deepmerge(acc, obj, { clone: false }));
-
+export const mergeSection = (ruleset, name, sectionName, entry) =>
+  Object.assign(
+    {},
+    deepmerge(ruleset[name][sectionName], entry, { clone: false, customMerge })
+  );
+export const mergeList = (list) =>
+  list.reduce((acc, obj) => deepmerge(acc, obj, { clone: false }));
 
 /*
   What is all this crap for?
@@ -69,17 +75,17 @@ export const mergeList = list => list.reduce((acc, obj) => deepmerge(acc, obj, {
 
 function traverse(x, cb, path = []) {
   cb(x, path);
-  
-  if(!Array.isArray(x) && (typeof x !== "object" || x === null)) {
+
+  if (!Array.isArray(x) && (typeof x !== "object" || x === null)) {
     return;
   }
-  
-  if(Array.isArray(x)) {
+
+  if (Array.isArray(x)) {
     x.forEach((item, i) => {
       traverse(item, cb, [...path, i]);
     });
   } else {
-    Object.keys(x).forEach(key => {
+    Object.keys(x).forEach((key) => {
       traverse(x[key], cb, [...path, key]);
     });
   }
@@ -92,40 +98,45 @@ export function findTags(result) {
   }, {});
 
   traverse(result, (item, path) => {
-    if(!item) return;
+    if (!item) return;
 
-    tags.forEach(tag => {
-      if(item[Symbol.for(tag)]) {
+    tags.forEach((tag) => {
+      if (item[Symbol.for(tag)]) {
         tagPaths[symbolToTag[Symbol.for(tag)]].push(path);
       }
     });
   });
 
   const tagsFound = tags.reduce((acc, tag) => {
-    acc += tagPaths[tag].length; 
+    acc += tagPaths[tag].length;
     return acc;
   }, 0);
 
   return tagsFound ? tagPaths : null;
 }
 
-
 // TODO: Make this async if we need to shift parsing to workers.
-export function parse(text) {
+export function parse(text, url) {
+  try {
     const result = yaml.load(text, { json: true, schema });
     return dehydrate(result);
+  } catch (e) {
+    e.url = url;
+    throw e;
+  }
 }
 
 export function hydrate([tree, tagPaths]) {
-  if(!tagPaths) return tree;
+  if (!tagPaths) return tree;
   Object.entries(tagPaths).forEach(([tag, paths]) => {
-    paths.forEach(path => {
+    paths.forEach((path) => {
       let node = tree;
-      for(let pathKey of path) { //walk tree to target object
+      for (let pathKey of path) {
+        //walk tree to target object
         node = node[pathKey];
       }
       node[Symbol.for(tag)] = true;
-    })
+    });
   });
   return tree;
 }
@@ -135,5 +146,12 @@ export function dehydrate(result) {
   return [result, tagPaths];
 }
 
-const defaultExports = { dehydrate, hydrate, findTags, mergeSection, mergeList, parse };
+const defaultExports = {
+  dehydrate,
+  hydrate,
+  findTags,
+  mergeSection,
+  mergeList,
+  parse,
+};
 export default defaultExports;
