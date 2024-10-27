bbe022160dc964e06a66f84f7a6ccf7d111903fb
noblebright <noblebright@github.com>
2024-10-27 00:54:42 -0700

    Temp fix for branch list until page traversal works.
    Works for 100 branches or less.

--- src/model/GithubLoader.js
+++ src/model/GithubLoader.js
@@ -12,10 +12,10 @@ export default class GithubLoader {
 
     async loadVersions(branchName = "master") {
         const [branches, tags] = await Promise.all([
-            loadJSON(`https://api.github.com/repos/${this.repoName}/branches`, true),
+            loadJSON(`https://api.github.com/repos/${this.repoName}/branches?per_page=100`, true),
             loadJSON(`https://api.github.com/repos/${this.repoName}/tags`, true)
         ]);
-        
+
         const branch = branches.find(x => x.name === branchName)
         this.versions = {
             [branchName]: branch.commit
@@ -33,23 +33,23 @@ export default class GithubLoader {
         const treeUrl = commitData.commit.tree.url;
         const treeData = await loadJSON(`${treeUrl}?recursive=true`, true);
         const fileList = treeData.tree;
-        
+
         const languageFiles = {};
         const ruleFiles = {};
         const foldCase = {};
 
         fileList.forEach(file => {
-            if(startPaths && !startPaths.find(x => file.path.startsWith(x))) {
+            if (startPaths && !startPaths.find(x => file.path.startsWith(x))) {
                 return;
             }
-            
+
             // normalize case so we can properly rewrite asset paths
             foldCase[file.path.toLowerCase()] = file.path;
 
-            if(file.path.match(/Language\/.*\.yml/)) {
+            if (file.path.match(/Language\/.*\.yml/)) {
                 languageFiles[file.sha] = this.getUrl(sha, file.path);
             }
-            if(file.path.match(/.*\.rul$/)) {
+            if (file.path.match(/.*\.rul$/)) {
                 ruleFiles[file.sha] = this.getUrl(sha, file.path);
             }
         });
