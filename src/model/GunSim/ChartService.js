5e7bec91ec812d57554ca8e8adcc26eca256607c
noblebright <noblebright@github.com>
2025-07-22 01:02:02 -0700

    Fix shotgun hit computation for shotgunBehavior: true
    Fixes #5

--- src/model/GunSim/ChartService.js
+++ src/model/GunSim/ChartService.js
@@ -1,6 +1,6 @@
 import DamageSimulator from "./DamageSimulator.js";
 import { unitWidths, ShotType } from "../Constants.js";
-import { computeAccuracyInputs } from "./Simulator.js";
+import { computeAccuracyInputs, getShotgunMultipler } from "./Simulator.js";
 import { mergeStats } from "./utils.js";
 import { truncateEpsilon } from "../utils.js";
 
@@ -8,180 +8,262 @@ import { truncateEpsilon } from "../utils.js";
 import { instantiate } from "../wasmInterface.js";
 
 function hasCost(value, suffix) {
-    const costObj = value[`cost${suffix}`];
-    const tu = value[`tu${suffix}`];
+  const costObj = value[`cost${suffix}`];
+  const tu = value[`tu${suffix}`];
 
-    return (costObj && costObj.time) || tu;
+  return (costObj && costObj.time) || tu;
 }
 
 const actionTypes = Object.values(ShotType);
 
 function getShotTypes(damageModel) {
-    const weapon = damageModel.weaponEntry;
-    const ammo = damageModel.ammoEntry;
-    if(!ammo || !weapon.ammo) { // ie, vanilla-style compatibleAmmo
-        return actionTypes.filter(type => hasCost(weapon, type));
-    } else {
-        const validAmmoSlots = new Set();
-        Object.entries(weapon.ammo).forEach(([key, value]) => {
-            if(value.compatibleAmmo.includes(ammo.type)) {
-                validAmmoSlots.add(key);
-            }
-        });
-        return actionTypes.filter((type, idx) => {
-            if(!hasCost(weapon, type)) {
-                return false;
-            }
-            if(!weapon[`conf${type}`]) {
-                return validAmmoSlots.has("0");
-            }
-            return validAmmoSlots.has(`${weapon[`conf${type}`].ammoSlot}`);
-        });
-    }    
+  const weapon = damageModel.weaponEntry;
+  const ammo = damageModel.ammoEntry;
+  if (!ammo || !weapon.ammo) {
+    // ie, vanilla-style compatibleAmmo
+    return actionTypes.filter((type) => hasCost(weapon, type));
+  } else {
+    const validAmmoSlots = new Set();
+    Object.entries(weapon.ammo).forEach(([key, value]) => {
+      if (value.compatibleAmmo.includes(ammo.type)) {
+        validAmmoSlots.add(key);
+      }
+    });
+    return actionTypes.filter((type, idx) => {
+      if (!hasCost(weapon, type)) {
+        return false;
+      }
+      if (!weapon[`conf${type}`]) {
+        return validAmmoSlots.has("0");
+      }
+      return validAmmoSlots.has(`${weapon[`conf${type}`].ammoSlot}`);
+    });
+  }
 }
 
 function getAttemptsPerTurn(ruleset, state, key = "weapon") {
-    const {soldier, armor, stat} = state;
-    const entries = ruleset.entries;
-    const weaponEntry = entries[state[key]].items;
-    const soldierEntry = entries[soldier].soldiers ?? entries[soldier].units; //if not a soldier, it's a vanilla hwp
-    const armorEntry = entries[armor].armors;
-    const soldierStats = soldierEntry[stat] ?? soldierEntry["stats"];
-    const adjustedStats = mergeStats(soldierStats, armorEntry.stats);
-    const tu = adjustedStats.tu;
-    const result = {};
+  const { soldier, armor, stat } = state;
+  const entries = ruleset.entries;
+  const weaponEntry = entries[state[key]].items;
+  const soldierEntry = entries[soldier].soldiers ?? entries[soldier].units; //if not a soldier, it's a vanilla hwp
+  const armorEntry = entries[armor].armors;
+  const soldierStats = soldierEntry[stat] ?? soldierEntry["stats"];
+  const adjustedStats = mergeStats(soldierStats, armorEntry.stats);
+  const tu = adjustedStats.tu;
+  const result = {};
 
-    actionTypes.forEach(type => {
-        const tuCost = hasCost(weaponEntry, type);
-        const flatCost = weaponEntry.flatRate || weaponEntry[`flat${type}`]?.time;
-        if(tuCost) {
-            result[type] = flatCost ? Math.floor(tu / tuCost) : Math.floor(100 / tuCost);
-        }
-    });
-    return result;
+  actionTypes.forEach((type) => {
+    const tuCost = hasCost(weaponEntry, type);
+    const flatCost = weaponEntry.flatRate || weaponEntry[`flat${type}`]?.time;
+    // TODO: Account for reload time
+    if (tuCost) {
+      result[type] = flatCost
+        ? Math.floor(tu / tuCost)
+        : Math.floor(100 / tuCost);
+    }
+  });
+  return result;
 }
 
 let accuracyData;
 let getTTK;
 
 async function loadWASM() {
-    const stream = await fetch("/release.wasm");
-    const wasm = await WebAssembly.compileStreaming(stream);
-    return instantiate(wasm, {});
+  const stream = await fetch("/release.wasm");
+  const wasm = await WebAssembly.compileStreaming(stream);
+  return instantiate(wasm, {});
 }
 
 export function loadDependencies() {
-    // load accuracy into buffer
-    const accuracy = fetch("/accuracyLookup.dat")
-            .then(response => response.arrayBuffer())
-            .then(buffer => { accuracyData = new Uint16Array(buffer)});
+  // load accuracy into buffer
+  const accuracy = fetch("/accuracyLookup.dat")
+    .then((response) => response.arrayBuffer())
+    .then((buffer) => {
+      accuracyData = new Uint16Array(buffer);
+    });
 
-    // load WASM code
-    const wasm = loadWASM().then(iface => { getTTK = iface.getTTK; window.getTTK = getTTK; });
+  // load WASM code
+  const wasm = loadWASM().then((iface) => {
+    getTTK = iface.getTTK;
+    window.getTTK = getTTK;
+  });
 
-    // wait for both
-    return Promise.all([accuracy, wasm]);
+  // wait for both
+  return Promise.all([accuracy, wasm]);
 }
 
 const widthOffsets = Object.values(unitWidths).reduce((acc, width, idx) => {
-    acc[width] = idx;
-    return acc;
+  acc[width] = idx;
+  return acc;
 }, {});
 
 function lookupAcc(accuracyData, targetWidth, targetHeight, acc, distance) {
-    const offset =  widthOffsets[targetWidth] * 132000 + // 50 * 110 * 24
-                    (targetHeight - 1) * 5500 + //height 1-24
-                    acc * 50 +      //acc 0-109
-                    (distance - 1); //distance 1-50
+  const offset =
+    widthOffsets[targetWidth] * 132000 + // 50 * 110 * 24
+    (targetHeight - 1) * 5500 + //height 1-24
+    acc * 50 + //acc 0-109
+    (distance - 1); //distance 1-50
 
-    return accuracyData[offset] * 100 /  65535;
+  return (accuracyData[offset] * 100) / 65535;
 }
 
 export function getChartData(ruleset, state) {
-    const damageModel = new DamageSimulator(ruleset, state);
-    const shotTypes = getShotTypes(damageModel);
-    // attempt = 1 mouse click, vs shot = 1 bullet/pellet
-    const attemptsPerTurnByType = getAttemptsPerTurn(ruleset, state);
+  const damageModel = new DamageSimulator(ruleset, state);
+  const shotTypes = getShotTypes(damageModel);
+  // attempt = 1 mouse click, vs shot = 1 bullet/pellet
+  const attemptsPerTurnByType = getAttemptsPerTurn(ruleset, state);
 
-    const compareDamageModel = state.compare && new DamageSimulator(ruleset, state, "compareWeapon", "compareAmmo");
-    const compareShotTypes = state.compare && getShotTypes(compareDamageModel);
-    const compareAttemptsPerTurnByType = state.compare && getAttemptsPerTurn(ruleset, state, "compareWeapon");
+  const compareDamageModel =
+    state.compare &&
+    new DamageSimulator(ruleset, state, "compareWeapon", "compareAmmo");
+  const compareShotTypes = state.compare && getShotTypes(compareDamageModel);
+  const compareAttemptsPerTurnByType =
+    state.compare && getAttemptsPerTurn(ruleset, state, "compareWeapon");
 
-    const data = [];
+  const data = [];
 
-    for(let distance = 1; distance <= 50; distance++) {
-        const dataPoint = { distance };
-        for(let shotType of shotTypes) {
-            const accuracyParameters = computeAccuracyInputs(ruleset, shotType, distance, state);
-            const [shotCount, ...accuracyInputs] = accuracyParameters;
-            const singleHitAccuracy = lookupAcc(accuracyData, ...accuracyInputs);
-            const hitRatio = singleHitAccuracy * shotCount;
-            const attemptsPerTurn = attemptsPerTurnByType[shotType];
-            const rangeModifiedDamage = damageModel.getRangeModifiedDamage(distance);
-            
-            dataPoint[`${shotType}HitRatio`] = hitRatio;
-            dataPoint[`${shotType}Damage`] = rangeModifiedDamage * hitRatio / 100 * attemptsPerTurn;
-            // function getTTK(health: i32, armor: i32, rolls: i32, lowLimit: i32, highLimit: i32, dmg: i32, hitChance: f64) : f64[] {
-            console.log(`${shotType}TTK_${distance}`, ...damageModel.TTKParams, singleHitAccuracy / 100);
-            const ttk = getTTK(shotCount, ...damageModel.TTKParams, damageModel.getTTKPower(distance), singleHitAccuracy / 100);
-            dataPoint[`${shotType}TTK`] = ttk.length;
-            for(let i = 1; i < ttk.length; i++) {
-                dataPoint[`${shotType}TTK_${i}`] = truncateEpsilon(ttk[i]);
-            }
-        }
-        if(state.compare) {
-            for(let shotType of compareShotTypes) {
-                const accuracyParameters = computeAccuracyInputs(ruleset, shotType, distance, state, "compareWeapon", "compareAmmo");
-                const [shotCount, ...accuracyInputs] = accuracyParameters;
-                const singleHitAccuracy = lookupAcc(accuracyData, ...accuracyInputs);
-                const hitRatio = singleHitAccuracy * shotCount;
-                const compareAttemptsPerTurn = compareAttemptsPerTurnByType[shotType];
-                const rangeModifiedDamage = compareDamageModel.getRangeModifiedDamage(distance);
-                
-                dataPoint[`Compare${shotType}HitRatio`] = hitRatio;
-                dataPoint[`Compare${shotType}Damage`] = rangeModifiedDamage * hitRatio / 100 * compareAttemptsPerTurn;
-                const ttk = getTTK(shotCount, ...compareDamageModel.TTKParams, compareDamageModel.getTTKPower(distance), singleHitAccuracy / 100);
-                dataPoint[`Compare${shotType}TTK`] = ttk.length;
-                for(let i = 1; i < ttk.length; i++) {
-                    dataPoint[`Compare${shotType}TTK_${i}`] = truncateEpsilon(ttk[i]);
-                }
+  const shotgunMultiplier = getShotgunMultipler(ruleset, state); // will just return 1 for non-shotgun weapons
+  const compareShotgunMultiplier = getShotgunMultipler(
+    ruleset,
+    state,
+    "compareWeapon",
+    "compareAmmo"
+  ); // will just return 1 for non-shotgun weapons
 
-            }
-        }
-        data.push(dataPoint);
+  for (let distance = 1; distance <= 50; distance++) {
+    const dataPoint = { distance };
+    for (let shotType of shotTypes) {
+      const accuracyParameters = computeAccuracyInputs(
+        ruleset,
+        shotType,
+        distance,
+        state
+      );
+      const [shotCount, ...accuracyInputs] = accuracyParameters;
+      const singleHitAccuracy = lookupAcc(accuracyData, ...accuracyInputs);
+
+      const hitRatio = singleHitAccuracy * shotCount * shotgunMultiplier;
+      const attemptsPerTurn = attemptsPerTurnByType[shotType];
+      const rangeModifiedDamage = damageModel.getRangeModifiedDamage(distance);
+      dataPoint[`${shotType}HitRatio`] = hitRatio;
+      dataPoint[`${shotType}Damage`] =
+        ((rangeModifiedDamage * hitRatio) / 100) * attemptsPerTurn;
+      // function getTTK(health: i32, armor: i32, rolls: i32, lowLimit: i32, highLimit: i32, dmg: i32, hitChance: f64) : f64[] {
+      console.log(
+        `${shotType}TTK_${distance}`,
+        ...damageModel.TTKParams,
+        singleHitAccuracy / 100
+      );
+      const ttk = getTTK(
+        Math.floor(shotCount * shotgunMultiplier), //HACK: ttk doesn't handle partial hits, so just round down.
+        ...damageModel.TTKParams,
+        damageModel.getTTKPower(distance),
+        singleHitAccuracy / 100
+      );
+      dataPoint[`${shotType}TTK`] = ttk.length;
+      for (let i = 1; i < ttk.length; i++) {
+        dataPoint[`${shotType}TTK_${i}`] = truncateEpsilon(ttk[i]);
+      }
     }
-    window.chartData = data;
-    return { data, weaponEntry: damageModel.weaponEntry, compareWeaponEntry: compareDamageModel?.weaponEntry };
+    if (state.compare) {
+      for (let shotType of compareShotTypes) {
+        const accuracyParameters = computeAccuracyInputs(
+          ruleset,
+          shotType,
+          distance,
+          state,
+          "compareWeapon",
+          "compareAmmo"
+        );
+        const [shotCount, ...accuracyInputs] = accuracyParameters;
+        const singleHitAccuracy = lookupAcc(accuracyData, ...accuracyInputs);
+        const hitRatio =
+          singleHitAccuracy * shotCount * compareShotgunMultiplier;
+        const compareAttemptsPerTurn = compareAttemptsPerTurnByType[shotType];
+        const rangeModifiedDamage =
+          compareDamageModel.getRangeModifiedDamage(distance);
+
+        dataPoint[`Compare${shotType}HitRatio`] = hitRatio;
+        dataPoint[`Compare${shotType}Damage`] =
+          ((rangeModifiedDamage * hitRatio) / 100) * compareAttemptsPerTurn;
+        const ttk = getTTK(
+          Math.floor(shotCount * compareShotgunMultiplier), //HACK: ttk doesn't handle partial hits, so just round down.
+          ...compareDamageModel.TTKParams,
+          compareDamageModel.getTTKPower(distance),
+          singleHitAccuracy / 100
+        );
+        dataPoint[`Compare${shotType}TTK`] = ttk.length;
+        for (let i = 1; i < ttk.length; i++) {
+          dataPoint[`Compare${shotType}TTK_${i}`] = truncateEpsilon(ttk[i]);
+        }
+      }
+    }
+    data.push(dataPoint);
+  }
+  window.chartData = data;
+  return {
+    data,
+    weaponEntry: damageModel.weaponEntry,
+    compareWeaponEntry: compareDamageModel?.weaponEntry,
+  };
 }
 
-window.getChartData = function (statMode, direction, soldier, armor, weapon, ammo, target, kneeling = false, oneHanded = false) {
-    if(!arguments.length) {
-        console.log([
-            "usage: getChartData(statMode, direction, soldier, armor, weapon, ammo, target, kneeling = false, oneHanded = false)",
-            "   statMode: one of 'minStats', 'maxStats', 'statCaps', 'trainingStatCaps'",
-            "   direction: one of 'front', 'left', 'right', 'rear'",
-            "   soldier: String key of ruleset entry with soldiers section",
-            "   armor: String key of ruleset entry with armor section",
-            "   weapon: String key of ruleset entry with items section and battleType: 1",
-            "   ammo: String key of ruleset entry with items section and battleType: 2",
-            "   target: String key of ruleset entry with unit section",
-            "   kneeling: boolean",
-            "   oneHanded: boolean"
-        ].join("\n"));
+window.getChartData = function (
+  statMode,
+  direction,
+  soldier,
+  armor,
+  weapon,
+  ammo,
+  target,
+  kneeling = false,
+  oneHanded = false
+) {
+  if (!arguments.length) {
+    console.log(
+      [
+        "usage: getChartData(statMode, direction, soldier, armor, weapon, ammo, target, kneeling = false, oneHanded = false)",
+        "   statMode: one of 'minStats', 'maxStats', 'statCaps', 'trainingStatCaps'",
+        "   direction: one of 'front', 'left', 'right', 'rear'",
+        "   soldier: String key of ruleset entry with soldiers section",
+        "   armor: String key of ruleset entry with armor section",
+        "   weapon: String key of ruleset entry with items section and battleType: 1",
+        "   ammo: String key of ruleset entry with items section and battleType: 2",
+        "   target: String key of ruleset entry with unit section",
+        "   kneeling: boolean",
+        "   oneHanded: boolean",
+      ].join("\n")
+    );
+    return;
+  }
+  const result = getChartData(window.db.ruleset, {
+    stat: statMode,
+    direction,
+    soldier,
+    armor,
+    weapon,
+    ammo,
+    compare: false,
+    compareWeapon: {},
+    compareAmmo: {},
+    target,
+    kneeling,
+    oneHanded,
+  });
+  const rows = {};
+  result.forEach((col) => {
+    Object.keys(col).forEach((k) => {
+      if (k === "distance") {
+        // skip distance
         return;
-    }
-    const result = getChartData(window.db.ruleset, { stat: statMode, direction, soldier, armor, weapon, ammo, compare: false, compareWeapon: {}, compareAmmo: {}, target, kneeling, oneHanded });
-    const rows = {};
-    result.forEach(col => {
-        Object.keys(col).forEach(k => {
-            if(k === "distance") { // skip distance
-                return;
-            }
-            if(!rows[k]) {
-                rows[k] = [];
-            }
-            rows[k].push(col[k]);
-        });
+      }
+      if (!rows[k]) {
+        rows[k] = [];
+      }
+      rows[k].push(col[k]);
     });
-    return Object.entries(rows).map(([k, v]) => [`${weapon}|${ammo}|${k}`, ...v].join(",")).join("\n");
-}
+  });
+  return Object.entries(rows)
+    .map(([k, v]) => [`${weapon}|${ammo}|${k}`, ...v].join(","))
+    .join("\n");
+};
--- src/model/GunSim/Simulator.js
+++ src/model/GunSim/Simulator.js
@@ -2,100 +2,156 @@ import { getMultiplier } from "./Multipliers.js";
 import { mergeStats } from "./utils.js";
 import { unitWidths, ShotType } from "../Constants.js";
 
-function getAccuracy(ruleset, stats, weapon, shotType, kneeling, oneHanded, distance) {
-    let acc = weapon[`accuracy${shotType}`];
-    let modifier = 0.0;
-    let upperLimit;
-	let lowerLimit = weapon.minRange ?? 0;
+function getAccuracy(
+  ruleset,
+  stats,
+  weapon,
+  shotType,
+  kneeling,
+  oneHanded,
+  distance
+) {
+  let acc = weapon[`accuracy${shotType}`];
+  let modifier = 0.0;
+  let upperLimit;
+  let lowerLimit = weapon.minRange ?? 0;
 
-	switch(shotType) {
-	case ShotType.Aimed:
-		upperLimit = weapon.aimRange ?? 200;
-		break;
-	case ShotType.Snap:
-		upperLimit = weapon.snapRange ?? 15;
-		break;
-	case ShotType.Auto:
-			upperLimit = weapon.autoRange ?? 7;
-		break;
-	default:
-	}
-    
-    const dropoff = weapon.dropoff ?? 2;
-	if(distance < lowerLimit) {
-		modifier = (dropoff * (lowerLimit - distance));
-	} else if (upperLimit < distance) {
-		modifier = (dropoff * (distance - upperLimit));
-	}
+  switch (shotType) {
+    case ShotType.Aimed:
+      upperLimit = weapon.aimRange ?? 200;
+      break;
+    case ShotType.Snap:
+      upperLimit = weapon.snapRange ?? 15;
+      break;
+    case ShotType.Auto:
+      upperLimit = weapon.autoRange ?? 7;
+      break;
+    default:
+  }
 
-	acc = Math.max(0, acc - modifier);
+  const dropoff = weapon.dropoff ?? 2;
+  if (distance < lowerLimit) {
+    modifier = dropoff * (lowerLimit - distance);
+  } else if (upperLimit < distance) {
+    modifier = dropoff * (distance - upperLimit);
+  }
 
-    if(weapon.accuracyMultiplier) {
-        acc *= getMultiplier(weapon.accuracyMultiplier, stats) / 100;
-    } else {
-        acc *= stats.firing / 100;
-    }
-    if(kneeling) {
-        acc *= (weapon.kneelBonus ?? ruleset.globalVars.kneelBonusGlobal ?? 115) / 100;
-    }
-    if(oneHanded && weapon.twoHanded) {
-        acc *= (weapon.oneHandedPenalty ?? ruleset.globalVars.oneHandedPenaltyGlobal ?? 80) / 100;
-    }
+  acc = Math.max(0, acc - modifier);
 
-    //cap accuracy at 109 since that's the max the game supports
-    return Math.min(Math.floor(acc), 109);
+  if (weapon.accuracyMultiplier) {
+    acc *= getMultiplier(weapon.accuracyMultiplier, stats) / 100;
+  } else {
+    acc *= stats.firing / 100;
+  }
+  if (kneeling) {
+    acc *=
+      (weapon.kneelBonus ?? ruleset.globalVars.kneelBonusGlobal ?? 115) / 100;
+  }
+  if (oneHanded && weapon.twoHanded) {
+    acc *=
+      (weapon.oneHandedPenalty ??
+        ruleset.globalVars.oneHandedPenaltyGlobal ??
+        80) / 100;
+  }
+
+  //cap accuracy at 109 since that's the max the game supports
+  return Math.min(Math.floor(acc), 109);
 }
 
 function getShots(weapon, ammo, mode) {
-    let shots = 1;
-    if(mode === "Auto") {
-        if(weapon.autoShots && !weapon.confAuto) {
-            shots = weapon.autoShots;
-        } else if(weapon.confAuto) {
-            shots = weapon.confAuto.shots;
-        } else {
-            shots = 3;
-        }
-    } else if(weapon[`conf${mode}`]) {
-        shots = weapon[`conf${mode}`].shots
+  let shots = 1;
+  if (mode === "Auto") {
+    if (weapon.autoShots && !weapon.confAuto) {
+      shots = weapon.autoShots;
+    } else if (weapon.confAuto) {
+      shots = weapon.confAuto.shots;
+    } else {
+      shots = 3;
     }
+  } else if (weapon[`conf${mode}`]) {
+    shots = weapon[`conf${mode}`].shots;
+  }
 
-    let pellets = 1;
-    let shotgunSpread = 100;
-    if(weapon.shotgunPellets) {
-        pellets = weapon.shotgunPellets;
-        shotgunSpread = weapon.shotgunSpread;
-    }
-    if(ammo?.shotgunPellets) {
-        pellets = ammo.shotgunPellets;
-        shotgunSpread = ammo.shotgunSpread;
-    }
-
-    // FIXME: Handle shotgunBehavior and shotgunChoke
-    // We approximate pellets as the expected value of shotgunSpread in shots.
-    // According to: https://openxcom.org/forum/index.php/topic,4834.0.html
-    // shotgunSpread:  Defined on an ammunition type as a number between 0 and 100 with a default value of 100.  
-    // With shotgunBehavior: true, this is approximatley the percent of pellets after the first that will hit 
-    // the same tile/target as the first at the maximum accurate range."
-    return shots * Math.max(1, Math.ceil(pellets * shotgunSpread / 100));
+  return shots;
 }
 
-export function computeAccuracyInputs(ruleset, shotType, distance, state, weaponKey = "weapon", ammoKey = "ammo") {
-    const {stat, soldier, armor, target, kneeling, oneHanded} = state;
-    const entries = ruleset.entries;
-    const weaponEntry = entries[state[weaponKey]].items;
-    const targetEntry = entries[target].units;
-    const soldierEntry = entries[soldier].soldiers ?? entries[soldier].units;
-    const armorEntry = entries[armor].armors;
-    const ammoEntry = entries[state[ammoKey]]?.items;
-    const soldierStats = soldierEntry[stat] ?? soldierEntry["stats"];
-    const adjustedStats = mergeStats(soldierStats, armorEntry.stats);
-    const acc = getAccuracy(ruleset, adjustedStats, weaponEntry, shotType, kneeling, oneHanded, distance);
-    const shots = getShots(weaponEntry, ammoEntry, shotType);
-    
-    const targetArmor = entries[targetEntry.armor].armors;
-    const targetHeight = targetEntry.standHeight;
-    const targetWidth = unitWidths[targetArmor.loftempsSet[0]] ?? 7; //weird-shaped units we assume are 7 voxels wide (human)
+export function getShotgunMultipler(
+  ruleset,
+  state,
+  weaponKey = "weapon",
+  ammoKey = "ammo"
+) {
+  const entries = ruleset.entries;
+  const weapon = entries[state[weaponKey]].items;
+  const ammo = entries[state[ammoKey]]?.items;
 
-    return [shots, targetWidth, targetHeight, acc, distance];
+  let pellets = 1;
+  let shotgunSpread = 100;
+  let shotgunChoke = 100;
+
+  if (weapon.shotgunPellets) {
+    pellets = weapon.shotgunPellets;
+    if (weapon.shotgunChoke !== undefined) {
+      shotgunChoke = weapon.shotgunChoke;
+    }
+  }
+  if (ammo?.shotgunPellets) {
+    pellets = ammo.shotgunPellets;
+    if (ammo.shotgunSpread !== undefined) {
+      shotgunSpread = ammo.shotgunSpread;
+    }
+  }
+
+  // FIXME: Handle shotgunBehavior:false mode
+  // We approximate pellets as the expected value of shotgunSpread in shots.
+  // According to: https://openxcom.org/forum/index.php/topic,4834.0.html
+  // shotgunSpread:  Defined on an ammunition type as a number between 0 and 100 with a default value of 100.
+  // With shotgunBehavior: true, this is approximatley the percent of pellets after the first that will hit
+  // the same tile/target as the first at the maximum accurate range."
+  const multiplier =
+    1 +
+    Math.max(
+      // pellets beyond the first * (100 - spread) * choke%
+      (pellets - 1) * (100 - shotgunSpread) * 0.01 * shotgunChoke * 0.01,
+      0
+    );
+  console.log(
+    `1 + Math.max((${pellets} - 1) * (100 - ${shotgunSpread}) * 0.01 * ${shotgunChoke} * 0.01, 0) = ${multiplier}`
+  );
+  return multiplier;
+}
+
+export function computeAccuracyInputs(
+  ruleset,
+  shotType,
+  distance,
+  state,
+  weaponKey = "weapon",
+  ammoKey = "ammo"
+) {
+  const { stat, soldier, armor, target, kneeling, oneHanded } = state;
+  const entries = ruleset.entries;
+  const weaponEntry = entries[state[weaponKey]].items;
+  const targetEntry = entries[target].units;
+  const soldierEntry = entries[soldier].soldiers ?? entries[soldier].units;
+  const armorEntry = entries[armor].armors;
+  const ammoEntry = entries[state[ammoKey]]?.items;
+  const soldierStats = soldierEntry[stat] ?? soldierEntry["stats"];
+  const adjustedStats = mergeStats(soldierStats, armorEntry.stats);
+  const acc = getAccuracy(
+    ruleset,
+    adjustedStats,
+    weaponEntry,
+    shotType,
+    kneeling,
+    oneHanded,
+    distance
+  );
+  const shots = getShots(weaponEntry, ammoEntry, shotType);
+
+  const targetArmor = entries[targetEntry.armor].armors;
+  const targetHeight = targetEntry.standHeight;
+  const targetWidth = unitWidths[targetArmor.loftempsSet[0]] ?? 7; //weird-shaped units we assume are 7 voxels wide (human)
+
+  return [shots, targetWidth, targetHeight, acc, distance];
 }
